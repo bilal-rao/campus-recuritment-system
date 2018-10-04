d14fdf87929d7a2570bbce8ba352a6f66e1b1d21
Muneeb JS <m.muneeb@nextgeni.net>
2018-10-04 17:02:44 +0500

    Move errors from Alert to UI for signup

--- src/containers/signUp/signUp.js
+++ src/containers/signUp/signUp.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
-import {browserHistory} from 'react-router';
+import { browserHistory } from 'react-router';
 import NavBar from '../../components/navBar/navBar';
 
 // import Paper from 'material-ui/Paper';
@@ -18,68 +18,73 @@ import NavBar from '../../components/navBar/navBar';
 
 
 class SignUp extends Component {
-        constructor(){
-            super();
-            this.state = {
-                user : null
-            }
-     }
+    constructor() {
+        super();
+        this.state = {
+            user: null,
+            error: null,
+        }
+    }
 
-     SignUp(ev){
-         ev.preventDefault();
-         var email = this.refs.email.value;
-         var password = this.refs.password.value;
-         var name = this.refs.name.value;
-         console.log(email);
-         console.log(password);
-        const auth=firebase.auth();
-       auth.createUserWithEmailAndPassword(email,password).catch(function(error){alert(error);
-    }).then(()=>{firebase.auth().currentUser.updateProfile({
-           displayName: name
-       });console.log(firebase.auth().currentUser);
-        browserHistory.push("/login"); 
-        var rootRef = firebase.database().ref("user" + "/" + firebase.auth().currentUser.uid ).set({
-            uName : name,
-            uEmail : email,
-            uPassword : password,
-            typeOfUser : this.state.user
+    SignUp(ev) {
+        ev.preventDefault();
+        var email = this.refs.email.value;
+        var password = this.refs.password.value;
+        var name = this.refs.name.value;
+        console.log(email);
+        console.log(password);
+        const auth = firebase.auth();
+        auth.createUserWithEmailAndPassword(email, password).catch((error) => {
+            //    alert(error);
+            this.setState({ error: error.message })
+        }).then(() => {
+            firebase.auth().currentUser.updateProfile({
+                displayName: name
+            }); console.log(firebase.auth().currentUser);
+            browserHistory.push("/login");
+            var rootRef = firebase.database().ref("user" + "/" + firebase.auth().currentUser.uid).set({
+                uName: name,
+                uEmail: email,
+                uPassword: password,
+                typeOfUser: this.state.user
+            });
+
+            // this.props.handlstate(this.state.user)
         });
 
-    // this.props.handlstate(this.state.user)
-    });
-
-     }
-     setUser(event){
+    }
+    setUser(event) {
         this.setState({
-            user : event.target.value
+            user: event.target.value
         });
-     }
-  render() {
-    return (
-      <div>
-         <center> <h1>SignUp</h1> </center>
-     <div className="container">
-        <form onSubmit={this.SignUp.bind(this)}>
-            <div className="form-group">
-                <label htmlFor="text">Enter Name:</label>
-                <input type="text" className="form-control" placeholder="Enter Name" ref="name"/> <br />
-            </div>
-            <div className="form-group">
-                <label htmlFor="email">Email:</label>
-                <input type="email" className="form-control" placeholder="Email" ref="email"/> <br/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">password:</label>
-                <input type="password" className="form-control" ref="password" placeholder="password" /> <br/>
-            </div>
+    }
+    render() {
+        return (
+            <div>
+                <center> <h1>SignUp</h1> </center>
+                <div className="container">
+                    <form onSubmit={this.SignUp.bind(this)}>
+                        <div className="form-group">
+                            <label htmlFor="text">Enter Name:</label>
+                            <input type="text" className="form-control" placeholder="Enter Name" ref="name" /> <br />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" className="form-control" placeholder="Email" ref="email" /> <br />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">password:</label>
+                            <input type="password" className="form-control" ref="password" placeholder="password" /> <br />
+                        </div>
 
-                <input className="btn btn-primary" type="submit" value="SignUp"/>
-                    <div className="radio" onChange={this.setUser.bind(this)}>
-                    <label><input type="radio" value="student" name="user"/>Student</label> <br/>
-                    <label><input type="radio" value="company" name="user"/>Company</label> <br/>
-                    
-                </div>
-            {/*<MuiThemeProvider>
+                        <input className="btn btn-primary" type="submit" value="SignUp" />
+                        {this.state.error && <div style={{ color: 'red' }}>{this.state.error}</div>}
+                        <div className="radio" onChange={this.setUser.bind(this)}>
+                            <label><input type="radio" value="student" name="user" />Student</label> <br />
+                            <label><input type="radio" value="company" name="user" />Company</label> <br />
+
+                        </div>
+                        {/*<MuiThemeProvider>
               <Paper 
               style={style} 
               zDepth={3} 
@@ -94,11 +99,11 @@ class SignUp extends Component {
                  ref="password"
               />
               </MuiThemeProvider>*/}
-        </form>
-         </div>
-      </div>
+                    </form>
+                </div>
+            </div>
 
-    );
-  }
+        );
+    }
 }
 export default SignUp;
